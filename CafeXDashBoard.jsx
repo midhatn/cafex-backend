2dc9a570dfa24defb3d50b99c5e2d9e10e2b48e7
midhatn <medhat.nashar@gmail.com>
2019-12-19 10:26:01 +0300

    README

--- CafeXDashBoard.jsx
+++ CafeXDashBoard.jsx
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
 
+
+
 export default class ViralDashBoard extends Component {
   render() {
-    const card = {
+    const card1 = {
       border: "3px solid white",
       borderRadius: "10px",
-      height: "150px",
-      width: "120px",
+      height: "300px",
+      width: "250px",
       margin: "0 auto",
-      marginLeft: "90px",
-      marginTop: "120px",
+      marginLeft: "200px",
+      marginTop: "200px",
+      marginRight: "200px",
+
       textAlign: "center",
       float: "left",
       align: "center"
     };
+    const card2 = {
+      border: "3px solid white",
+      borderRadius: "10px",
+      height: "330px",
+      width: "280px",
+      margin: "0 auto",
+      marginLeft: "1px",
+      marginTop: "200px",
+      marginRight: "250px",
+      textAlign: "center",
+      float: "right",
+      align: "center"
+    };
+
 
     return (
       <div>
@@ -21,22 +39,26 @@ export default class ViralDashBoard extends Component {
           <h1>Welcome To CafeX Page</h1>
           <br/>
           <br/>
-          <h2>Dev Team : </h2>
+      
         </div>
-        <div style={card}>
+      
+        <div style={card1}>
           <img
-            src=""
+            src="https://ca.slack-edge.com/TA2AHQDQ8-UNF1GF9LM-d75732949c6f-512"
             alt=""
           />
           <a href="https://github.com/midhatn">
             <h1>Midhat</h1>
           </a>
         </div>
-        <div style={card}>
+        <span style={{fontSize:400 ,color:"red"}}>&hearts;</span>         
+        <div style={card2}>
           <img
-            src=""
+            src="https://emoji.slack-edge.com/TA2AHQDQ8/anas2/46a1c8b3c962233d.png" 
             alt=""
+            height="768" width="768"
           />
+          
           <a href="https://github.com/Anas10895">
             <h1>Anas</h1>
           </a>
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# cafex-backend
+# GA-SEI-CafeX-Backend
+
+Backend for GA-SEI Project 4
+## Description
+
+Using MongoDB to Connect full functioning CRUD Server to Frontend of Cafex website
+
+### Technical Used
+
+```
+- Express Js
+- Bootstrap with react
+- Ajax (axios) for API
+- AdminBro
+- Google Maps API
+- MongoDB 
+- Passport
+- JsonWebToken
+- Bcrybt
+- Postman Collections
+-
+```
+
+### Wireframes for ERD
+
+![ERD](../ga-pro4-cafex-backend/erd.png)
+
+
+
+## Planning and Development Process
+
+Building the Database Structure was curcial and using the right toold to build the databse as NoSQL
+
+
+### Unsolved problems
+
+- Expanding The database to include more relations to remove any duplicated data
+
+
+## Acknowledgments
+
+
+---
+
+ ## References
+
+ Frontend : https://github.com/Anas10895/Steak
